dcc53befc3918a445dab6af2497cab39ffd98750
officiallycraft-boop <officiallycraft@gmail.com>
2026-03-31 23:56:44 +0100

    fix: Create timetable page that was missing

---
+++ app/dashboard/timetable/page.tsx
@@ -0,0 +1,8 @@
+'use client';
+
+import React from 'react';
+import TimetableEditor from '@/components/TimetableEditor';
+
+export default function TimetablePage() {
+  return <TimetableEditor orgId="" orgName="" />;
+}
